2741568f37bc82a18230016391a1394e716c238c
om prakash <omprakashkumarsantosh@gmail.com>
2025-02-03 20:43:15 +0530

    home page change ok5

--- backend_IT_Ticket_Management_System/server.js
+++ backend_IT_Ticket_Management_System/server.js
@@ -1,11 +1,21 @@
-const https = require('https');
+// const https = require('https');
+// const app = require('./app');
+
+// const PORT = process.env.PORT || 4000;
+
+// const server = https.createServer(app);
+ 
+// server.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// }
+// );
+const http = require('http');
 const app = require('./app');
 
 const PORT = process.env.PORT || 4000;
 
-const server = https.createServer(app);
- 
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-}
-);
+const server = http.createServer(app);
+
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
+});
