fd7c9352c432308bd67193317d9cf3165a28436d
om prakash <omprakashkumarsantosh@gmail.com>
2025-03-10 13:36:17 +0530

    change http into https

--- backend_IT_Ticket_Management_System/src/config/socket.js
+++ backend_IT_Ticket_Management_System/src/config/socket.js
@@ -1,9 +1,10 @@
 // src/config/socket.js
 const { Server } = require("socket.io");
-const http = require("http");
+// const http = require("http");
+const https = require("https");
 
 function initializeSocket(app) {
-  const server = http.createServer(app); // Attach Express app to server
+  const server = https.createServer(app); // Attach Express app to server
   const io = new Server(server, {
     cors: {
       origin: ["http://localhost:5173","https://it-ticket-management-system-om-app.vercel.app"],
