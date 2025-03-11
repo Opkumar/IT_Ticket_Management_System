f00c4603572758b74b7fc1b9ea4cb2359ce2131e
om prakash <omprakashkumarsantosh@gmail.com>
2025-03-11 22:01:07 +0530

    test19

--- frontend_IT_Ticket_Management_System/src/lib/axios.js
+++ frontend_IT_Ticket_Management_System/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://it-ticket-management-system-xi.vercel.app/api",
+    baseURL: "https://it-ticket-management-system.onrender.com/api",
     withCredentials:true,
 })
 
